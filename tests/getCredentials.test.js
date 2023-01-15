077482d65d4b638c436367999e4b9e265cc8e676
Orie Steele <orie@transmute.industries>
2023-01-15 10:26:49 -0600

    :rocket:

--- tests/getCredentials.test.js
+++ tests/getCredentials.test.js
@@ -3,19 +3,18 @@ const operationSwitch = require('../src');
 
 let accessToken = ''
 
-beforeAll(async ()=>{
-  accessToken = await operationSwitch({
-    operationId: 'getAccessToken',
-    did: process.env.ORGANIZATION_DID_WEB,
-    tokenEndpoint: process.env.TOKEN_ENDPOINT,
-    tokenAudience: process.env.TOKEN_AUDIENCE,
-    apiBaseUrl: process.env.API_BASE_URL,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  });
-})
-
-describe("getCredentials", () => {
+describe.skip("getCredentials", () => {
+  beforeAll(async ()=>{
+    accessToken = await operationSwitch({
+      operationId: 'getAccessToken',
+      did: process.env.ORGANIZATION_DID_WEB,
+      tokenEndpoint: process.env.TOKEN_ENDPOINT,
+      tokenAudience: process.env.TOKEN_AUDIENCE,
+      apiBaseUrl: process.env.API_BASE_URL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    });
+  })
   it("get all credentials", async () => {
     const response = await operationSwitch({
       operationId: 'getCredentials',
