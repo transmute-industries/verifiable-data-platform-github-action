79b9b647296cd1a4074f720b3616b9c5cabb54ab
Orie Steele <orie@transmute.industries>
2023-01-21 09:17:59 -0600

    Add service implementations

--- src/vdp.js
+++ src/vdp.js
@@ -44,6 +44,99 @@ const storeCredential = async ({apiBaseUrl, accessToken, verifiableCredential}) 
   }
 }
 
+const issueCredential = async ({apiBaseUrl, accessToken, credential}) => {
+  try {
+    const url = apiBaseUrl +  '/credentials/issue'
+    const res = await axios.post(url, { credential, "options": {
+      "type": "Ed25519Signature2018",
+      // "created": "2020-04-02T18:48:36Z",
+      // "credentialStatus": {
+      //   "type": "RevocationList2020Status"
+      // }
+    }}, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+    return res.data;
+  } catch(e){
+    console.error(e)
+    return null
+  }
+}
+
+const notifyPresentationAvailable = async ({  endpoint, query}) => {
+  try {
+    const res = await axios.post(endpoint, { query });
+    return res.data;
+  } catch(e){
+    console.error(e)
+    return null
+  }
+}
+
+const provePresentation = async ({apiBaseUrl, accessToken, presentation, domain, challenge}) => {
+  try {
+    const url = apiBaseUrl +  '/presentations/prove'
+    const body = { 
+      presentation, 
+      "options": {
+        domain,
+        challenge
+      }
+  }
+    const res = await axios.post(url, body, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+    return res.data;
+  } catch(e){
+    console.error(e)
+    return null
+  }
+}
+
+
+
+const sendTo = async ({apiBaseUrl, accessToken, contactId, presentation}) => {
+  try {
+    const url = apiBaseUrl +  '/presentations/send-did-auth-presentation'
+    const body = { 
+      presentation, 
+      contactId
+  }
+    const res = await axios.post(url, body, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+    return res.data;
+  } catch(e){
+    console.error(e)
+    return null
+  }
+}
+
+const submitPresentationWithOAuth2Security = async ({apiBaseUrl, accessToken, organizationId, presentation}) => {
+  try {
+    const url = apiBaseUrl +  `/organizations/${organizationId}/presentations`
+    const body = presentation;
+    const res = await axios.post(url, body, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      },
+    });
+    return res.data;
+  } catch(e){
+    console.error(e)
+    return null
+  }
+}
+
+
+
+
 const token = {
   get: getAccessToken
 }
@@ -51,9 +144,17 @@ const token = {
 const credentials = {
   get: getCredentials,
   store: storeCredential,
+  issue: issueCredential
 }
 
-const api = { token, credentials };
+const presentations = {
+  available: notifyPresentationAvailable,
+  prove: provePresentation, // broken
+  sendTo: sendTo,
+  submit: submitPresentationWithOAuth2Security
+}
+
+const api = { token, credentials, presentations };
 
 
 module.exports = api;
