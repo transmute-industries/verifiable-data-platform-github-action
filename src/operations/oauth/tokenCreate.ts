f3be24a3d0e144723a76a2d22b96684bbb1df785
Adam Carnagey <carnagey.adam@gmail.com>
2023-03-06 18:37:22 -0600

    fix: attempt 3

--- src/operations/oauth/tokenCreate.ts
+++ src/operations/oauth/tokenCreate.ts
@@ -1,15 +1,22 @@
-import * as core from '@actions/core';
-import { Api } from '../../vdp'; 
-import { axiosConfig } from '../../config';
+import * as core from "@actions/core";
+import { Api } from "../../vdp";
+import { axiosConfig } from "../../config";
 
 export const tokenCreate = async (env: any) => {
-    const api = new Api({ ...axiosConfig });
-    const { data: token } = await api.oauth.tokenCreate({
-      grant_type: 'client_credentials',
-      client_id: env.clientId,
-      client_secret: env.clientSecret,
-      audience: env.tokenAudience
-    });
-    core.exportVariable("verifiable_data_platform_access_token", token.access_token)
-    return null;
-}
+  const api = new Api({ ...axiosConfig });
+  const { data: token } = await api.oauth.tokenCreate({
+    grant_type: "client_credentials",
+    client_id: env.clientId,
+    client_secret: env.clientSecret,
+    audience: env.tokenAudience,
+  });
+  core.exportVariable(
+    "API_BASE_URL",
+    core.getInput("api-base-url") || process.env.API_BASE_URL
+  );
+  core.exportVariable(
+    "verifiable_data_platform_access_token",
+    token.access_token
+  );
+  return null;
+};
