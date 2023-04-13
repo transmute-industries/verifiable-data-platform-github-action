0d8c82f1999131b4655ddc3872901da4666e04ec
Orie Steele <orie@transmute.industries>
2023-04-13 18:54:56 -0500

    Fix lint

--- src/operations/workflowInstances/updateWorkflowInstance.ts
+++ src/operations/workflowInstances/updateWorkflowInstance.ts
@@ -3,6 +3,7 @@ import { Api } from '../../vdp';
 import { axiosConfig } from '../../config';
 import { generateHeaders } from '../../utils';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const updateWorkflowInstance = async ({ workflowInstanceId, workflowInstance }: any) => {
     const api = new Api({ ...axiosConfig });
     const headers = generateHeaders()
